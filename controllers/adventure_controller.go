a905556fc3bf55b37db67968e2fa91ef788fbe1b
Ian Lyell <ian.lyell@yahoo.com>
2023-10-16 10:26:58 -0500

    wip: fix small error

--- controllers/adventure_controller.go
+++ controllers/adventure_controller.go
@@ -17,12 +17,11 @@ import (
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
 	// "go.mongodb.org/mongo-driver/mongo"
 )
 
 var validateAdventure = validator.New()
-
+var adventureCollection collections.AdventureCollection
 
 func SetAdventureCollection(collection collections.AdventureCollection) {
     adventureCollection = collection
