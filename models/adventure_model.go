6b5c2b1f159e2af812e89c0826a10b00fd301230
Derek Chavez <frrenose17@yahoo.com>
2023-10-16 15:28:51 -0600

    wip: add omitempty to adventure models

--- models/adventure_model.go
+++ models/adventure_model.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Adventure struct {
     User_id              string `json:"user_id" binding:"required"`
-    Adventure_id         primitive.ObjectID `json:"adventure_id" bson:"_id"`
+    Adventure_id         primitive.ObjectID `json:"adventure_id,omitempty" bson:"_id,omitempty"`
     Activity             string `json:"activity" binding:"required"`
     Date                 string `json:"date,omitempty"`
     Image_url            string `json:"image_url,omitempty"`
