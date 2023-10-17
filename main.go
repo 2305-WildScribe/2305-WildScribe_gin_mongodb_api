0779dfa0c9f5a7ec52c190c32fa3107e2b59f5cf
Ian Lyell <ian.lyell@yahoo.com>
2023-10-17 18:06:03 -0500

    fix: Cors fix this time #7

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"gin-mongo-api/routes"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/gin-contrib/cors"
+	"github.com/gin-contrib/cors"
 	// "log"
 	"os"
 	// "net/http"
@@ -21,8 +21,13 @@ func main() {
 		routes.UserRoute(router)
 		// adventure_collection :
 		// same as
-
-		router.Use(CORS())
+		config := cors.DefaultConfig()
+		config.AllowAllOrigins = true
+		config.AllowOrigins = []string{"http://localhost:3000"}
+ 		config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+ 		config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization","Access-Control-Allow-Origin", "*","Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"}
+	
+		router.Use(cors.New(config))
 		// router.Use(cors.Default())
 		router.Run()
         routes.AdventureRoute(router)
@@ -32,19 +37,4 @@ func main() {
 		} else {
 			router.Run("localhost:6000")
 	}    
-}
-func CORS() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
 }
