4a270fceeb3884bd14c401f280d17a1485030480
Ian Lyell <ian.lyell@yahoo.com>
2023-10-17 17:37:19 -0500

    fix: Cors fix this time #5

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"gin-mongo-api/routes"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gin-contrib/cors"
+	// "github.com/gin-contrib/cors"
 	// "log"
 	"os"
 	// "net/http"
@@ -19,22 +19,36 @@ func main() {
 
 		configs.ConnectDB()
 		routes.UserRoute(router)
-
 		// adventure_collection :
 		// same as
-		config := cors.DefaultConfig()
-		config.AllowAllOrigins = true
-		config.AllowOrigins = []string{"http://localhost:3000"}
- 		config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
- 		config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
-		router.Use(cors.New(config))
+		// config := cors.DefaultConfig()
+		// config.AllowAllOrigins = true
+		// config.AllowOrigins = []string{"http://localhost:3000"}
+ 		// config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+ 		// config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+		router.Use(CORSMiddleware())
 		// router.Use(cors.Default())
 		router.Run()
         routes.AdventureRoute(router)
-				if os.Getenv("PROD_ENV") == "production" {
-				port := os.Getenv("PORT")
-				router.Run(":"+port)
-				} else {
-					router.Run("localhost:6000")
-				}    
+		if os.Getenv("PROD_ENV") == "production" {
+			port := os.Getenv("PORT")
+			router.Run(":"+port)
+		} else {
+			router.Run("localhost:6000")
+	}    
+}
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
 }
