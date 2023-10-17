6d1f9e7014b73d7a5508cd91a79d0412edc10a2a
Ian Lyell <ian.lyell@yahoo.com>
2023-10-17 18:26:38 -0500

    fix: This time Cors #9

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 		config := cors.DefaultConfig()
 		// config.AllowAllOrigins = true
 		config.AllowOrigins = []string{"http://localhost:3000"}
-		router.Use(cors.New(config))
+		router.Use(CORSMiddleware())
 		router.Run()
         routes.AdventureRoute(router)
 		if os.Getenv("PROD_ENV") == "production" {
@@ -31,4 +31,21 @@ func main() {
 		} else {
 			router.Run("localhost:6000")
 	}    
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Content-Type", "application/json")
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+        c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+        } else {
+            c.Next()
+        }
+    }
 }
