386763514d4d63525ef95ff422155e3e4d9b8132
Ian Lyell <ian.lyell@yahoo.com>
2023-10-17 15:44:31 -0500

    test: test cors fix

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 
 	// "log"
 	"os"
+	"net/http"
+	"github.com/rs/cors"
 	// "github.com/joho/godotenv"
 )
 
@@ -18,8 +20,30 @@ func main() {
 
 		configs.ConnectDB()
 		routes.UserRoute(router)
+
 		// adventure_collection :
 		// controllers.SetAdventureCollection(adventure_collection)
+ // Create an HTTP server
+ 	// c := cors.New(cors.Options{
+    // AllowedOrigins:   []string{"http://localhost:3000"},
+    // AllowCredentials: true,
+    // AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+	// })
+
+	// handler := c.Handler(mux)
+	server := http.NewServeMux()
+
+	// Define your HTTP handler function
+	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Set the necessary headers to allow cross-origin requests
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Change * to your specific origin(s)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+		// Your API logic here
+	})
+
+	http.ListenAndServe(":8080", server)
 
         routes.AdventureRoute(router)
 				if os.Getenv("PROD_ENV") == "production" {
